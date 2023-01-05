13451a77e5bf6d5155defca724b096e87c82bc49
tristero <crf8472@web.de>
2023-01-05 21:21:59 +0100

    Fix g++ warning about unused result value

--- src/tools-parse.cpp
+++ src/tools-parse.cpp
@@ -62,7 +62,8 @@ std::vector<char> StdIn::bytes()
 	// https://msdn.microsoft.com/en-us/library/tw4k6df8.aspx
 #else
 
-	std::freopen(nullptr, "rb", stdin); // ignore returned FILE ptr to stdin
+	static_cast<void>(std::freopen(nullptr, "rb", stdin));
+	// ignore returned FILE ptr to stdin while avoiding warning
 #endif
 
 	// Binary Mode From Here On
