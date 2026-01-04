c4ff33860169f3ae373c93389ead0b58af20ba91
tristero <crf8472@mailbox.org>
2026-01-04 21:19:33 +0100

    Add testcase for Modifier

---
+++ test/src/ansi.cpp
@@ -0,0 +1,40 @@
+#include "catch2/catch_test_macros.hpp"
+
+#include <vector>
+
+#ifndef __ARCSTOOLS_ANSI_HPP__
+#include "ansi.hpp"
+#endif
+
+/**
+ * \file
+ *
+ * \brief Fixtures for ansi.cpp
+ */
+
+// TODO reset()
+// TODO get_color()
+// TODO colored()
+
+TEST_CASE ( "Modifier", "[modifier]" )
+{
+	using arcsapp::ansi::Color;
+	using arcsapp::ansi::Highlight;
+	using arcsapp::ansi::Modifier;
+
+	auto m1 = Modifier { Highlight::BOLD, { Color::FG_BLACK, Color::BG_RED } };
+	auto m2 = Modifier { Highlight::UNDERL };
+
+	SECTION ( "Default construction works" )
+	{
+		CHECK ( m1.highlight() == Highlight::BOLD );
+		CHECK ( m1.colors()    ==
+				std::vector<Color>{ Color::FG_BLACK, Color::BG_RED } );
+		CHECK ( m1.str()       == "\x1B[1;30;41m" );
+
+		CHECK ( m2.highlight() == Highlight::UNDERL );
+		CHECK ( m2.colors()    == std::vector<Color>{/*empty*/} );
+		CHECK ( m2.str()       == "\x1B[4m" );
+	}
+}
+
