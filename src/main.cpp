3823b04ece8b4330eafe634c50ccb8e2bb2a5065
tristero <crf8472@mailbox.org>
2026-01-07 21:56:17 +0100

    Log exceptions different than logged ERRORs

--- src/main.cpp
+++ src/main.cpp
@@ -95,7 +95,7 @@ int main(int argc, char** argv)
 
 			} catch (const std::exception& e)
 			{
-				std::cerr << "ERROR: " << e.what() << '\n';
+				std::cerr << "Exception: " << e.what() << '\n';
 				return EXIT_FAILURE;
 			}
 		} else
