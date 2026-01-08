9ee51747f381cbc7558436d70e26238e7fc889e4
tristero <crf8472@mailbox.org>
2026-01-08 16:22:00 +0100

    Minor change

--- test/src/app-id.cpp
+++ test/src/app-id.cpp
@@ -14,8 +14,9 @@
 TEST_CASE ( "ARIdConfigurator", "[ARIdConfigurator]" )
 {
 	using arcsapp::ARIdConfigurator;
-	using arcsapp::OPTION;
 	using arcsapp::ARIdOptions;
+	using arcsapp::FORMATBASE;
+	using arcsapp::OPTION;
 
 	SECTION ("List of supported options is sound and complete")
 	{
@@ -26,25 +27,56 @@ TEST_CASE ( "ARIdConfigurator", "[ARIdConfigurator]" )
 
 		CHECK ( 18 == supported.size() );
 
-		CHECK ( contains(ARIdOptions::READERID, supported) );
-		CHECK ( contains(ARIdOptions::PARSERID, supported) );
-		CHECK ( contains(ARIdOptions::LIST_TOC_FORMATS, supported) );
-		CHECK ( contains(ARIdOptions::LIST_AUDIO_FORMATS, supported) );
-		CHECK ( contains(ARIdOptions::CDDBID, supported) );
-		CHECK ( contains(ARIdOptions::DBID, supported) );
-		CHECK ( contains(ARIdOptions::FILENAME, supported) );
-		CHECK ( contains(ARIdOptions::NOLABELS, supported) );
-		CHECK ( contains(ARIdOptions::URL, supported) );
-		CHECK ( contains(ARIdOptions::PROFILE, supported) );
-		CHECK ( contains(ARIdOptions::URLPREFIX, supported) );
-		CHECK ( contains(ARIdOptions::AUDIOFILE, supported) );
+		// OPTION::NONE is not a cli-supported option
+		CHECK ( contains(OPTION::HELP,                    supported) );
+		CHECK ( contains(OPTION::VERSION,                 supported) );
+		CHECK ( contains(OPTION::VERBOSITY,               supported) );
+		CHECK ( contains(OPTION::QUIET,                   supported) );
+		CHECK ( contains(OPTION::LOGFILE,                 supported) );
+		CHECK ( contains(OPTION::OUTFILE,                 supported) );
 
-		CHECK ( contains(OPTION::HELP, supported) );
-		CHECK ( contains(OPTION::VERSION, supported) );
-		CHECK ( contains(OPTION::VERBOSITY, supported) );
-		CHECK ( contains(OPTION::QUIET, supported) );
-		CHECK ( contains(OPTION::LOGFILE, supported) );
-		CHECK ( contains(OPTION::OUTFILE, supported) );
+		CHECK ( contains(FORMATBASE::LIST_TOC_FORMATS,    supported) );
+		CHECK ( contains(FORMATBASE::LIST_AUDIO_FORMATS,  supported) );
+		CHECK ( contains(FORMATBASE::READERID,            supported) );
+		CHECK ( contains(FORMATBASE::PARSERID,            supported) );
+
+		CHECK ( contains(ARIdOptions::CDDBID,             supported) );
+		CHECK ( contains(ARIdOptions::URL,                supported) );
+		CHECK ( contains(ARIdOptions::DBID,               supported) );
+		CHECK ( contains(ARIdOptions::FILENAME,           supported) );
+		CHECK ( contains(ARIdOptions::PROFILE,            supported) );
+		CHECK ( contains(ARIdOptions::URLPREFIX,          supported) );
+		// ARIdOptions::ID is not a cli-supported option
+		CHECK ( contains(ARIdOptions::AUDIOFILE,          supported) );
+		CHECK ( contains(ARIdOptions::NOLABELS,           supported) );
+
+		//
+		CHECK ( ! contains(ARIdOptions::ID,               supported) );
+		CHECK ( ! contains(OPTION::NONE,                  supported) );
 	}
+}
+
+
+TEST_CASE ( "ARIdApplication", "[aridapplication]" )
+{
+	using arcsapp::ARIdApplication;
+	using arcsapp::ARIdConfigurator;
+
+	auto app = ARIdApplication {/*default*/};
+
+	SECTION ( "Name is 'id'" )
+	{
+		CHECK ( "id" == app.name() );
+	}
+
+	// TODO test following functions
+	// call_syntax()
+	// create_configurator()
+
+	// TODO test following functions with default config
+	// calculation_requested()
+	// requested_types()
+	// run_calculation()
+
 }
 
